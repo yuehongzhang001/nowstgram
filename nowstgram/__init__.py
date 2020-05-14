1e75459919fbced35566e67d4372a70cdb3b82ab
Yuehong Zhang <ftdforthedream@hotmail.com>
2020-05-14 09:57:33 -0500

    pages display finished

--- nowstgram/__init__.py
+++ nowstgram/__init__.py
@@ -2,12 +2,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app=Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 app.config.from_pyfile('app.conf')
 db = SQLAlchemy(app)
-
+app.secret_key = 'nowcoder'
+login_manager = LoginManager(app);
 
 from nowstgram import views,models
 
--- nowstgram/views.py
+++ nowstgram/views.py
@@ -1,8 +1,10 @@
 # -*- encoding=UTF-8 -*-
 
-from nowstgram import app
+from nowstgram import app, db
 from models import Image,User,Comment
-from flask import render_template, redirect
+from flask import render_template, redirect,request, flash, get_flashed_messages
+import random, hashlib
+
 
 @app.route('/')
 def index():
@@ -22,3 +24,41 @@ def profile(user_id):
     if user == None:
         return redirect('/')
     return render_template('profile.html', user=user)
+
+@app.route('/regloginpage')
+def regloginpage():
+    msg = ''
+    for m in get_flashed_messages(with_categories=False, category_filter=['reglogin']):
+        msg = msg + m
+    return render_template('login.html', msg=msg)
+
+def redirect_with_msg(target,msg,category):
+    if msg != None:
+        flash(msg, category=category)
+    return redirect(target)
+
+
+@app.route('/reg/', methods={'post','get'})
+def reg():
+    username=request.values.get('username').strip()
+    password = request.values.get('password').strip()
+    # check if the username is empty
+    if username == '' or password == '':
+       return redirect_with_msg('/regloginpage',u'用户名或密码不能为空',category='reglogin')
+
+    #check if the username exists or not
+    user = User.query.filter_by(username=username).first()
+    if user != None:
+        return redirect_with_msg('/regloginpage',u'用户名已存在',category='reglogin')
+
+    # create the user and store it in the database
+    salt = '.'.join(random.sample('0123456789abcdefghijklmnopqrstuvwxyz',10))
+    m = hashlib.md5()
+    m.update(password+salt)
+    password = m.hexdigest()
+    user = User(username,password,salt)
+    db.session.add(user)
+    db.session.commit()
+
+    #redirect to the index page
+    return redirect('/')
